ca0904ae45e1667d821e0aaf28e5b1020f556cfd
Lauriita <laurahb123@gmail.com>
2020-01-15 16:10:13 +0100

    Agregar archivos de proyecto.

---
+++ Form1.vb
@@ -0,0 +1,26 @@
+﻿Public Class Form1
+
+    Dim a(5, 5) As Double
+    Dim i, j As Integer
+    Dim cadena As String
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        For i = 0 To 4
+            For j = 0 To 4
+
+                If i = j Then
+                    a(i, j) = 1
+                Else
+                    a(i, j) = 0
+                End If
+
+                cadena = cadena & a(i, j & " ")
+
+            Next
+
+            cadena = (cadena & vbNewLine)
+        Next
+        TextBox1.Text = cadena
+    End Sub
+End Class
